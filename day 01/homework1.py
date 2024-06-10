2bc3689ec7f195b88e6255ccb0084a653336264b
luka658976@gmail.com <luka658976@gmail.com>
2024-06-10 20:15:29 +0400

    first commit

---
+++ day 01/homework1.py
@@ -0,0 +1,50 @@
+print ("hello world")
+
+
+#homework no.1 come up with 5 F string equasions
+#no.1
+
+name_1 = "giorgi"
+name_2 = "saba"
+name_3 = "nick"
+
+sentience_1 = f"first human's name is {name_1}. second person's name is {name_2}. and the last persons name is {name_3}"
+
+
+print(sentience_1)
+
+#no.2
+
+title_1 = "slayer"
+title_2 = "admin"
+title_3 = "director"
+sentience_2 = f"first we aquired the title {title_1}. second title was {title_2}. and the last but not least the {title_3}."
+
+print(sentience_2)
+
+#no.3
+
+item_1 = "baseball bat"
+item_2 = "violin"
+item_3 = "blade"
+
+sentience_3 = f"number one item on our list is a {item_1}. the secont item is a {item_2}. and the last item is a {item_3}."
+print(sentience_3)
+
+#no.4
+
+element_1 = "fire"
+element_2 = "water"
+element_3 = "lightning"
+
+sentience_4 = f"this song is {element_1}. bring me some {element_2}. how to create {element_3}."
+print(sentience_4)
+
+#no.5
+
+location_1 = "rome"
+location_2 = "skopje"
+location_3 = "bucharest"
+
+sentience_5 = f"i whould like to visit {location_1} someday. who lives in {location_2}. where is {location_3}?"
+print(sentience_5)
---
+++ day 02/classwork1.py
@@ -0,0 +1,9 @@
+dog_name1 = "jhon"
+dog_name2 = "luke"
+dog_name2 = "sam"
+#print dogs names
+
+sentence=("first dog's name is" + " "+ dog_name1 + " " + "second dog's name is" + " "+dog_name2 + ".")
+sentence=f"first dog's name is {dog_name1}. second dog's name is {dog_name2}"
+
+print(sentence)
