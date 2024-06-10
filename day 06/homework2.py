cc1b1bf2be81bec07f50fa68cce4bbafcacc9576
luka658976@gmail.com <luka658976@gmail.com>
2024-06-10 20:49:10 +0400

    second commit

---
+++ day 06/homework2.py
@@ -0,0 +1,14 @@
+
+
+#name:
+user = input("enter your username: ")
+name = input("enter your name: ")
+gmail = input("please enter your gmail: ")
+password = input("please enter your password: ")
+
+print(name)
+print(gmail)
+print(password)
+
+
+
