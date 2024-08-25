b289ec33979288a56d751cf9f2c5e17768b4aea8
luka658976@gmail.com <luka658976@gmail.com>
2024-08-25 21:28:30 +0400

    second commit

---
+++ day 30/classwork.py
@@ -0,0 +1,7 @@
+existing_dict = {'key1': 'value1', 'key2': 'value2'}
+new_key = 'key3'
+new_value = 'value3'
+
+updated_dict = {**existing_dict, new_key: new_value}
+print(updated_dict)
+
---
+++ day 30/homework.py
@@ -0,0 +1,6 @@
+
+pairs_list = [('Lamborghini',1), ('Ford', 2), ('Alfa romeo', 3)]
+new_dict = {} 
+for key, value in pairs_list: 
+    new_dict[key] = value 
+print(new_dict) 
