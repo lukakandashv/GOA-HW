72f3a3049116f04ca2e4190cc19a2861daf50a65
lukakandashv <luka658976@gmail.com>
2025-02-04 06:01:37 -0800

    second commit

---
+++ day 75/classwork/reg/iwada.js
@@ -0,0 +1,9 @@
+function logSubmit(event) {
+    log.textContent = `Form Submitted! Timestamp: ${event.timeStamp}`;
+    event.preventDefault();
+  }
+  
+  const form = document.getElementById("form");
+  const log = document.getElementById("log");
+  form.addEventListener("submit", logSubmit);
+  
---
+++ day 75/homework/homework.js
@@ -0,0 +1,5 @@
+let age = 60;
+
+let res = (age > 59) ? "Adult" : "Not Adult";
+
+console.log(res);
